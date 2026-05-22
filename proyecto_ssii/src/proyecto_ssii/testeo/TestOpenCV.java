aa2200dea003a6cedb79e2c6e827652b1f38bb8c
ivanadd <ivan.arias@alumnos.upm.es>
2026-05-22 18:22:24 +0200

    [Test] Añadido testeo para la cámara.
    Solo saca una foto, no es constante, solo era para hacer una prueba.

--- proyecto_ssii/src/proyecto_ssii/testeo/TestOpenCV.java
+++ proyecto_ssii/src/proyecto_ssii/testeo/TestOpenCV.java
@@ -1,9 +1,18 @@
 package proyecto_ssii.testeo;
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
+import org.opencv.videoio.VideoCapture;
 
 public class TestOpenCV {
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println("OpenCV cargado.");
+        VideoCapture camera = new VideoCapture(0);
+        Mat frame = new Mat();
+        if(camera.read(frame)) {
+            HighGui.imshow("Camara", frame);
+            HighGui.waitKey(1);
+        }
     }
 }
