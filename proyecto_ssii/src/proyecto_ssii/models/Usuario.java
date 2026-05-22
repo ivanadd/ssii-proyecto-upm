f912af78dba385c8ce1ff66715e94fada97ac5cb
ivanadd <ivan.arias@alumnos.upm.es>
2026-05-22 20:08:30 +0200

    [v0.1] Primer modelaje de una clase Usuario para más adelante.

---
+++ proyecto_ssii/src/proyecto_ssii/models/Usuario.java
@@ -0,0 +1,12 @@
+package proyecto_ssii.models;
+
+public class Usuario {
+	private String nombre;
+	private String matricula;
+	
+	public Usuario(String nombre, String matricula) {
+		this.nombre = nombre;
+		this.matricula = matricula;
+	}
+	
+}
