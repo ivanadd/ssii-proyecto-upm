b9d24ff1b565da2ba4225c4a7171c86ccffd15d9
ivanadd <ivan.arias@alumnos.upm.es>
2026-05-22 14:28:30 +0200

    [Inicial] Actualizado pom.xml con posibles dependencias del proyecto

---
+++ proyecto_ssii/src/proyecto_ssii/agents/AgenteLector.java
@@ -0,0 +1,10 @@
+package proyecto_ssii.agents;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
+
+@SuppressWarnings("unused")
+public class AgenteLector extends Agent {
+	
+}
