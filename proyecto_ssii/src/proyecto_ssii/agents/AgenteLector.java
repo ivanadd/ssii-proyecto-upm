3ca4cc037c10df57af921d3d5ed9f372b2cfe2cf
ivanadd <ivan.arias@alumnos.upm.es>
2026-05-22 17:13:48 +0200

    [Inicial] Actualizado AgenteLector con comentarios para iniciar a trabajar

--- proyecto_ssii/src/proyecto_ssii/agents/AgenteLector.java
+++ proyecto_ssii/src/proyecto_ssii/agents/AgenteLector.java
@@ -4,7 +4,32 @@ import jade.core.Agent;
 import jade.core.behaviours.*;
 import jade.lang.acl.*;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({ "unused", "serial" })
 public class AgenteLector extends Agent {
-	
+	protected CyclicBehaviour cyclicBehaviour; 
+	protected void setup() {
+		cyclicBehaviour = new CyclicBehaviour(this) {
+			
+			@Override
+			public void action() {
+				// TODO Auto-generated method stub
+				// 1. Leer frame webcam
+				
+				
+				// 2. Detectar matricula
+				
+				
+				// 3. OCR
+				
+				
+				// 4. Dibujar overlay
+				
+				
+				// 5. Mostrar img
+				
+				
+			}
+		};
+		addBehaviour(cyclicBehaviour);
+	}
 }
