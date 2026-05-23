801ef30c79884f2e0d472e1ab6b06399d07991eb
Victor <victor.vergara@alumnos.upm.es>
2026-05-23 18:01:50 +0200

    AgenteInterfaz Tipos de accion

--- proyecto_ssii/src/proyecto_ssii/agents/AgenteInterfaz.java
+++ proyecto_ssii/src/proyecto_ssii/agents/AgenteInterfaz.java
@@ -1,5 +1,24 @@
 package proyecto_ssii.agents;
 
-public class AgenteInterfaz {
+import jade.core.Agent;
 
+@SuppressWarnings("serial")
+public class AgenteInterfaz extends Agent {
+
+    // Tipos de acción que llegan en el contenido del mensaje desde AgenteLogica
+    public static final String ACCESO_PERMITIDO  = "ACCESO_PERMITIDO";
+    public static final String ACCESO_DENEGADO   = "ACCESO_DENEGADO";
+    public static final String PEDIDO_EN_PROCESO = "PEDIDO_EN_PROCESO";
+
+
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
