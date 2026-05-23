7eb9d84282ffe40293da8f670af407a667e766c6
ivanadd <ivan.arias@alumnos.upm.es>
2026-05-23 16:59:44 +0200

    [v0.1] Añadido dni a usuario

--- proyecto_ssii/src/proyecto_ssii/models/Usuario.java
+++ proyecto_ssii/src/proyecto_ssii/models/Usuario.java
@@ -3,10 +3,12 @@ package proyecto_ssii.models;
 @SuppressWarnings("unused")
 public class Usuario {
 	private String nombre;
+	private String dni;
 	private String matricula;
 	
-	public Usuario(String nombre, String matricula) {
+	public Usuario(String nombre, String dni, String matricula) {
 		this.nombre = nombre;
+		this.dni = dni;
 		this.matricula = matricula;
 	}
 	
